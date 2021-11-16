9b5a405fd7bdff0e6f9e623f5d5295751bf344cd
mukul-kc <mukul@karmacircles.com>
2021-11-16 21:39:55 +0530

    add box serializer

--- crud_api/boxes/serializers.py
+++ crud_api/boxes/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
+from users.models import User
+from .models import Box
 
-class BoxSerializer(serializers.Serializer):
-    length = serializers.FloatField()
-    breadth = serializers.FloatField()
-    height = serializers.FloatField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
+class BoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__' 
+        model = Box
