09d9bb4ae119cacf2726b1c3c2cc57a4fa628d97
mukul-kc <mukul@karmacircles.com>
2021-11-16 21:39:34 +0530

    Finish POST :/api/boxes to create a box

--- crud_api/boxes/views.py
+++ crud_api/boxes/views.py
@@ -3,10 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Box
+from .serializers import BoxSerializer
+
 # Create your views here.
 class BoxView(APIView):
 
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         pass
@@ -14,6 +16,11 @@ class BoxView(APIView):
     def post(self, request, *args, **kwargs):
 
         user = request.user
+
+        if user.is_staff == False:
+            return Response({"error": ["You don't have sufficient permissions"]},
+                        status=status.HTTP_403_FORBIDDEN)
+
         l = request.data.get('length', None)
         b = request.data.get('breadth', None)
         h = request.data.get('height', None)
@@ -27,6 +34,5 @@ class BoxView(APIView):
         h = float(b)
 
         box = Box.objects.create(length=l, breadth=b, height=h, created_by = user)
-        print(box)
-
-        return Response(status=status.HTTP_200_OK)
+        
+        return Response(BoxSerializer(box).data,status=status.HTTP_200_OK)
