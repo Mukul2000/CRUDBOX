6a99e0a87594e11479ef2e809db3073e7d60f8c5
mukul-kc <mukul@karmacircles.com>
2021-11-16 21:49:43 +0530

    Finish PUT :/api/boxes/:id to update any box

--- crud_api/boxes/urls.py
+++ crud_api/boxes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/boxes', views.BoxView.as_view(), name='boxes')
+    path('api/boxes', views.BoxView.as_view(), name='boxes'),
+    path('api/boxes/<int:id>', views.BoxView.as_view(), name='box')
 ]
--- crud_api/boxes/views.py
+++ crud_api/boxes/views.py
@@ -36,3 +36,38 @@ class BoxView(APIView):
         box = Box.objects.create(length=l, breadth=b, height=h, created_by = user)
         
         return Response(BoxSerializer(box).data,status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+
+        user = request.user
+
+        if user.is_staff == False:
+            return Response({"error": ["You don't have sufficient permissions"]},
+                        status=status.HTTP_403_FORBIDDEN)
+
+        box_id = self.kwargs.get('id', None)
+
+        if box_id is None:
+            return Response({'id': ['This field is required']},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            box = Box.objects.get(id=box_id)
+        except Box.DoesNotExist:
+            return Response({'error': ['No box with this id']},
+                        status = status.HTTP_404_NOT_FOUND)
+
+        l = request.data.get('length', None)
+        h = request.data.get('breadth', None)
+        b = request.data.get('height', None)
+
+        if l is not None:
+            box.length = float(l)
+        if b is not None:
+            box.breadth = float(b) 
+        if h is not None:
+            box.height = float(h)
+
+        box.save()
+
+        return Response(BoxSerializer(box).data, status = status.HTTP_200_OK)
